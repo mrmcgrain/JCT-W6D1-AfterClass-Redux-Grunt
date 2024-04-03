b74bc0c86d95ede667cf5dd087fdd824caca2399
Michael McGrain <michael.r.mcgrain@gmail.com>
2024-04-03 15:34:18 -0700

    minified

--- Gruntfile.cjs
+++ Gruntfile.cjs
@@ -1,31 +1,42 @@
 module.exports = function(grunt) {
-    // Project configuration.
-    grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-      // Configure Browserify
-      browserify: {
-        options: {
-          transform: [
-            ['babelify', { presets: ['@babel/preset-env', '@babel/preset-react'] }]
-          ],
-          extensions: ['.jsx'], // Ensure Babelify applies to .jsx files
-          browserifyOptions: {
-            debug: true, // Enables source map
-            extensions: ['.jsx'] // Ensure Browserify processes .jsx files
-          }
-        },
-        app: {
-          src: 'src/main.jsx', // Adjusted for .jsx entry point
-          dest: 'dist/bundle.js'
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Browserify configuration
+    browserify: {
+      options: {
+        transform: [
+          ['babelify', { presets: ['@babel/preset-env', '@babel/preset-react'] }]
+        ],
+        extensions: ['.jsx'],
+        browserifyOptions: {
+          debug: true,
+          extensions: ['.jsx']
         }
+      },
+      app: {
+        src: 'src/main.jsx',
+        dest: 'dist/bundle.js'
       }
-      
-    });
-  
-    // Load the plugin that provides the "browserify" task.
-    grunt.loadNpmTasks('grunt-browserify');
-  
-    // Default task(s).
-    grunt.registerTask('default', ['browserify']);
-  };
-  
+    },
+
+    // Uglify configuration
+    uglify: {
+      options: {
+        // Use to add a banner or configure other options
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'dist/bundle.js',
+        dest: 'dist/bundle.min.js' // Output file after minification
+      }
+    }
+  });
+
+  // Load the Grunt plugins.
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Register default task.
+  grunt.registerTask('default', ['browserify', 'uglify']);
+};
